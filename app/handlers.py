7d42243d0918794620a0cf736f96b9eeceddd16b
dharkeGBC <d.harke@yandex.ru>
2025-01-27 11:51:05 +0300

    add chack subscription middleware

--- app/handlers.py
+++ app/handlers.py
@@ -4,30 +4,24 @@ from aiogram.filters import CommandStart, Command
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 from config import channel_id, channel_url
-
+from functools import wraps
 
 import app.keyboards as kb
 
 router = Router()
 
-async def check_subscription(message: Message):
-    # Получаем информацию о пользователе в чате
-    chat_member = await message.bot.get_chat_member(channel_id, message.from_user.id)
-
-    if chat_member.status in ['member', 'administrator', 'creator']:
-        # Пользователь подписан
-        return True
-    else:
-        # Пользователь не подписан
-        return False
-
-async def ask_for_subscription(message: Message):
-    # Создаем inline клавиатуру с кнопкой для перехода на канал
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="Перейти на канал", url=channel_url)]
-    ])
-    
-    await message.reply("Чтобы использовать этого бота, подпишись на канал:", reply_markup=keyboard)
+def check_subscription_middleware(func):
+    @wraps(func)
+    async def wrapper(message: Message, *args, **kwargs):
+        chat_member = await message.bot.get_chat_member(channel_id, message.from_user.id)
+        if chat_member.status in ['member', 'administrator', 'creator']:
+            return await func(message, *args, **kwargs)
+        else:
+            keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                [InlineKeyboardButton(text="Перейти на канал", url=channel_url)]
+            ])
+            return await message.answer("Чтобы использовать этого бота, подпишись на канал:", reply_markup=keyboard)
+    return wrapper
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
@@ -38,12 +32,10 @@ async def cmd_start(message: Message):
 На связи Харке Виктория Валентивна, КМН, Доцент.
 Данный бот отвечает на самые частые вопросы, задаваемые на ортодонтическом приёме, и также содержит полезную информацию для пациентов.
 ''', reply_markup=kb.main)
-    
+
+
 @router.message()
+@check_subscription_middleware
 async def handle_all_messages(message: Message):
-    if await check_subscription(message):
-        await message.bot.send_message(message.chat.id, f"Я вас не понимаю. Выберите один из пунктов.", reply_markup=kb.main)
-    else:
-        # Если не подписан, отправляем ссылку на канал
-        await ask_for_subscription(message)
+    await message.bot.send_message(message.chat.id, f"Я вас не понимаю. Выберите один из пунктов.", reply_markup=kb.main)
     
